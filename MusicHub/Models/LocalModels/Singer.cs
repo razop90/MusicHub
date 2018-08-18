940fe0a4065cb55331a255bfbb851cc9b7ec377f
Raz Vaknin <razop90@gmail.com>
2018-08-18 15:35:46 +0300

    Create singer,song and user models
    -Raz.

---
+++ MusicHub/Models/LocalModels/Singer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicHub.Models.LocalModels
+{
+    public class Singer
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public List<Song> Songs { get; set; }
+    }
+}
