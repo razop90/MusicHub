6de40778faa7977ff3aafd29890f667756305509
Raz Vaknin <razop90@gmail.com>
2018-08-18 15:51:28 +0300

    Arrange models.
    -Raz.

--- MusicHub/Models/LocalModels/Song.cs
+++ MusicHub/Models/LocalModels/Song.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,23 @@ namespace MusicHub.Models.LocalModels
 {
     public class Song
     {
+        #region Properties
+
+        [Key]
+        public int ID { get; set; }
+
         public string Name { get; set; }
         public Singer SingerDetails { get; set; }
         public MusicGenre Genre { get; set; }
         public string Composer { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string YouTubeUrl { get; set; }
+
+        #endregion
+
+        public Song()
+        {
+
+        }
     }
 }
--- MusicHub/Models/LocalModels/User.cs
+++ MusicHub/Models/LocalModels/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,20 @@ namespace MusicHub.Models.LocalModels
 {
     public class User
     {
+        #region Properties
+
         public string Name { get; set; }
         public string LastName { get; set; }
         public DateTime BirthDate { get; set; }
+        [Key]
         public string EmailAddress { get; set; }
         public List<Song> Playlist { get; set; }
+
+        #endregion
+
+        public User()
+        {
+
+        }
     }
 }
