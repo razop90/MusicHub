def3d9bea5385312313b1bfbea9cff1737be529f
Raz Vaknin <razop90@gmail.com>
2018-08-25 10:40:12 +0300

    hot fix

--- MusicHub/Interfaces/ISong.cs
+++ MusicHub/Interfaces/ISong.cs
@@ -9,7 +9,7 @@ namespace MusicHub.Interfaces
     public interface ISong
     {
         string Name { get; set; }
-        int ArtistId { get; set; }
+        int? ArtistId { get; set; }
         ArtistModel Artist { get; set; }
         MusicGenre Genre { get; set; }
         string Composer { get; set; }
