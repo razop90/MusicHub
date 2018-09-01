28e5746c61a7e331980252bd2923218759cccb1b
aliceka495 <aliceka495@gmail.com>
2018-09-01 19:22:36 +0300

    added 'Songs' in index and added authonticaion

--- MusicHub/Controllers/SongController.cs
+++ MusicHub/Controllers/SongController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using MusicHub.Models.LocalModels;
 
 namespace MusicHub.Controllers
 {
+    [Authorize]
     public class SongController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -20,6 +22,7 @@ namespace MusicHub.Controllers
         }
 
         // GET: Song
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.Songs.Include(s => s.Artist);
